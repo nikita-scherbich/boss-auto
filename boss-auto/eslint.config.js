b1657808636ea658668b17788d31d5c780f10777
Nikita Scherbich <nikita.scherbich@gmail.com>
2026-04-27 12:02:28 +0200

    chore: upgrade ESLint configuration with React and accessibility plugins and add root element validation

--- boss-auto/eslint.config.js
+++ boss-auto/eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -11,12 +13,25 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.strict,
+      ...tseslint.configs.stylistic,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
+      jsxA11y.flatConfigs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    rules: {
+      'react/jsx-no-leaked-render': ['error', { validStrategies: ['coerce'] }],
+      'react/self-closing-comp': 'error',
+    },
     languageOptions: {
       globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: '19.0',
+      },
     },
   },
 ])
